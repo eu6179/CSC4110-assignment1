a5fe67f47e295fd6cb3e1f1849486cb49ecf519d
Fatima Hashmi <eu6179@wayne.edu>
2020-09-16 11:34:01 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Fatima Hashmi <eu6179@wayne.edu> 
+
 #include<iostream>  
   
 using namespace std; 
